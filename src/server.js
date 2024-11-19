2ec24900a9ec0e249097b32f5c19715e870a11f3
opilane <aleks09.teki5@gmail.com>
2024-11-19 16:33:17 +0200

    [Delivers #188557056] login

---
+++ src/server.js
@@ -0,0 +1,39 @@
+import sirv from 'sirv';
+import polka from 'polka';
+import compression from 'compression';
+import * as sapper from '@sapper/server';
+import session from 'express-session';
+import sessionFileStore from 'session-file-store';
+const { json } = require('body-parser');
+
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
+const FileStore = new sessionFileStore(session);
+
+
+polka() // You can also use Express
+	.use(
+		compression({ threshold: 0 }),
+		json(),
+		session({
+			secret: 'aleks',
+			resave: true,
+			saveUninitialized: true,
+			cookie: {
+				maxAge: 31536000
+			},
+			store: new FileStore({
+				path: `.sessions`
+			})
+		}),
+		sirv('static', { dev }),
+		sapper.middleware({
+			session: req => ({
+				user: req.session && req.session.user,
+				token: req.session && req.session.token
+			})
+		})
+	)
+	.listen(PORT, err => {
+		if (err) console.log('error', err);
+	});
